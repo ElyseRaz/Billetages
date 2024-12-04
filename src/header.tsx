695cb0e9e3f45e0984b8ba3f50f9b27bd490cb7d
ElyseRaz <elyseherwin@gmail.com>
2024-12-04 16:18:57 +0300

    Version2

--- src/header.tsx
+++ src/header.tsx
@@ -1,7 +1,7 @@
 function Header() {
     return (
         <div >
-            <img src="./src/assets/billet-dargent.png" alt="logo-billet-dargent" className="w-16 mx-auto mb-4" />
+            <img src="/src/assets/billet-dargent.png" alt="logo-billet-dargent" className="w-16 mx-auto mb-4" />
             <h1 className="text-3xl font-bold font-sans text-center text-blue-600 ">Application Billetage </h1>
         </div>
     )
